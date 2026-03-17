877dc07a1d64910fe739970e386b9e9699b6f061
LUCA GAMAS DE MAIO <49008593@ort>
2026-03-17 14:20:38 -0300

    conectados inputs de html con js

---
+++ popup.js
@@ -0,0 +1,8 @@
+const companyUser=document.getElementById("companyUser")
+const companyPassword=document.getElementById("companyPassword")
+const AreaUser=document.getElementById("AreaUser")
+const AreaPassword=document.getElementById("AreaPassword")
+
+
+
+
